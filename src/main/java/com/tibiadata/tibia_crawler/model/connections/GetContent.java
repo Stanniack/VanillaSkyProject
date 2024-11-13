09dc491c589068c2cb8f28d3c1ed8689913b9bb2
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-13 20:24:36 -0300

    GetContent: Atualizando método getTableContent
    - Alterando retorno para List<String>

--- src/main/java/com/tibiadata/tibia_crawler/model/connections/GetContent.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/connections/GetContent.java
@@ -17,11 +17,11 @@ public class GetContent {
      * @param url link to get HTTP connection
      * @param select1 HTML tag1 ex: table, tbody, tr, td and its selectors
      * @param select2 HTML tag2 ex: table, tbody, tr, td and its selectors
-     * @return a list of string lists containing all
+     * @return a list of string containing all
      * &lt;tag1&gt;&lt;tag2&gt;&lt;/tag2&gt;&lt;/tag1&gt; elements.
      * @throws IOException
      */
-    public List<List<String>> getTableContent(String url, String select1, String select2) throws IOException {
+    public List<String> getTableContent(String url, String select1, String select2) throws IOException {
         Document document = Jsoup.connect(url).get(); // Pega o html
 
         String html = document.toString(); // HTML a ser limpo
@@ -29,15 +29,12 @@ public class GetContent {
         // Fazer parsing do HTML
         Document parserDocument = Jsoup.parse(html);
 
-        List<List<String>> allContent = new ArrayList<>();
+        List<String> allContent = new ArrayList<>();
 
         for (Element tr : parserDocument.select(select1)) {
-            List<String> eachTag = new ArrayList<>();
-
             for (Element td : tr.select(select2)) {
-                eachTag.add(td.text()); // Adiciona o texto de cada à lista
+                allContent.add(td.text()); // Adiciona o texto de cada à lista
             }
-            allContent.add(eachTag);
         }
         return allContent;
     }
