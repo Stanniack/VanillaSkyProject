d314960d2a064c0cc785fe8e8fa503b0f31ea648
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-13 20:27:59 -0300

    Update TibiaCrawlerApplication.java

--- src/main/java/com/tibiadata/tibia_crawler/TibiaCrawlerApplication.java
+++ src/main/java/com/tibiadata/tibia_crawler/TibiaCrawlerApplication.java
@@ -1,13 +1,19 @@
 package com.tibiadata.tibia_crawler;
 
+import com.tibiadata.tibia_crawler.model.scripts.GetCharacter;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TibiaCrawlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TibiaCrawlerApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        ApplicationContext context = SpringApplication.run(TibiaCrawlerApplication.class, args);
+        GetCharacter getCharacter = context.getBean(GetCharacter.class);
+
+        getCharacter.getCharacter("https://www.tibia.com/community/?subtopic=characters&name=Vepeeh");
+    }
 
 }
