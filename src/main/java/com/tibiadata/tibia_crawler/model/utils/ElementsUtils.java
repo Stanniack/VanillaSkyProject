8d08a8b31436e1b6625a06f14f63f3c0f288f636
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-12 21:20:56 -0300

    ElementUtils: Adicionando classe utils
    - Adicionando gets para retornar elements para seleção de tags e selectors

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/ElementsUtils.java
@@ -0,0 +1,29 @@
+package com.tibiadata.tibia_crawler.model.utils;
+
+/**
+ *
+ * @author Devmachine
+ */
+public class ElementsUtils {
+
+    private final String td = "td";
+    private final String tr = "tr";
+    private final String trEvenOdd = "tr.Even, tr.Odd";
+    private final String trBgcolor = "tr[bgcolor='#F1E0C6'], tr[bgcolor='#D4C0A1']";
+
+    public String getTd() {
+        return td;
+    }
+
+    public String getTrEvenOdd() {
+        return trEvenOdd;
+    }
+
+    public String getTrBgcolor() {
+        return trBgcolor;
+    }
+
+    public String getTr() {
+        return tr;
+    }
+}
