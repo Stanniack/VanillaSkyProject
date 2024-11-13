67274251ec634b092703f2c88f65674441e792e4
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-13 20:25:45 -0300

    Camada de persistência: FormerName e Personage
    - Adicionando duas camadas de persistência para caso seja necessário criar métodos alternativos

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/persistence/FormerNamePersistence.java
@@ -0,0 +1,23 @@
+package com.tibiadata.tibia_crawler.model.persistence;
+
+import com.tibiadata.tibia_crawler.model.entities.FormerName;
+import com.tibiadata.tibia_crawler.model.repositories.FormerNameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Devmachine
+ */
+@Component
+public class FormerNamePersistence {
+
+    @Autowired
+    FormerNameRepository fnr;
+
+    public FormerName save(FormerName fn) {
+        return fnr.save(fn);
+    }
+    
+
+}
---
+++ src/main/java/com/tibiadata/tibia_crawler/model/persistence/PersonagePersistence.java
@@ -0,0 +1,30 @@
+package com.tibiadata.tibia_crawler.model.persistence;
+
+import com.tibiadata.tibia_crawler.model.entities.Personage;
+import com.tibiadata.tibia_crawler.model.repositories.PersonageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Devmachine
+ */
+@Component
+public class PersonagePersistence {
+
+    @Autowired
+    PersonageRepository pp;
+
+    public Personage save(Personage p) {
+        return pp.save(p);
+    }
+
+    public Personage findByName(String name) {
+        return pp.findByName(name);
+    }
+    
+    public boolean existsByName(String name) {
+        return pp.existsByName(name);
+    }
+
+}
