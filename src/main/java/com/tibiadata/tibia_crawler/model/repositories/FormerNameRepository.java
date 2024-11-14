180df6e021b517825dc1da961202436a07b20499
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-14 20:28:34 -0300

    FormerNameRepository: Eliminando métodos
    - Eliminando métodos não utilizados

--- src/main/java/com/tibiadata/tibia_crawler/model/repositories/FormerNameRepository.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/repositories/FormerNameRepository.java
@@ -10,5 +10,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface FormerNameRepository extends JpaRepository<FormerName, Long> {
-    
+
+    FormerName findByFormerName(String formerName);
 }
