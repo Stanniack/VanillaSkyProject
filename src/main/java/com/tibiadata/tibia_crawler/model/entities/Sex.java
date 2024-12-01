76caf9716939c6397f4a6510e1faa68823481f8c
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-01 02:45:02 -0300

    Adicionando Classe entidade Sex

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/entities/Sex.java
@@ -0,0 +1,72 @@
+package com.tibiadata.tibia_crawler.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Devmachine
+ */
+@Entity
+public class Sex {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String genre;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", nullable = false) // FK será associada ao "id" de Personage
+    private Personage personage;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar untilDateOf;
+
+    /**
+     * Hibernate needs a default constructor
+     */
+    public Sex() {
+    }
+
+    public Sex(String genre, Calendar untilDateOf) {
+        this.genre = genre;
+        this.untilDateOf = untilDateOf;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Calendar getUntilDateOf() {
+        return untilDateOf;
+    }
+
+    public void setUntilDateOf(Calendar untilDateOf) {
+        this.untilDateOf = untilDateOf;
+    }
+
+    public Personage getPersonage() {
+        return personage;
+    }
+
+    public void setPersonage(Personage personage) {
+        this.personage = personage;
+    }
+
+}
