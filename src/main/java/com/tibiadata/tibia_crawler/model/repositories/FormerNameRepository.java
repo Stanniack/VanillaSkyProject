a709d5b2ac3ccb1e3bf62b0397bcbbaebb84fb09
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-05 02:30:00 -0300

    FormerNameRepository: Adicionando método findDateOfLastFormerNameRegistered
    - Adicionando findDateOfLastFormerNameRegistered com native query

--- src/main/java/com/tibiadata/tibia_crawler/model/repositories/FormerNameRepository.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/repositories/FormerNameRepository.java
@@ -1,6 +1,7 @@
 package com.tibiadata.tibia_crawler.model.repositories;
 
 import com.tibiadata.tibia_crawler.model.entities.FormerName;
+import java.util.Date;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,5 +18,9 @@ public interface FormerNameRepository extends JpaRepository<FormerName, Long> {
 
     @Query("SELECT COUNT(fn) > 0 FROM FormerName fn WHERE fn.formerName = :formerName AND fn.personage.id = :personageID")
     boolean isFormerNameFromPersonage(@Param("formerName") String formerName, @Param("personageID") Integer personageID);
+
+    @Query(value = "SELECT until_date_of FROM former_name fn JOIN Personage p on fn.parent_id = p.id WHERE former_name = :formerName ORDER BY until_date_of DESC LIMIT 1",
+            nativeQuery = true)
+    Date findDateOfLastFormerNameRegistered(@Param("formerName") String formerName);
 
 }
