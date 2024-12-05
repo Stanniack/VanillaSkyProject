55e69022b3033b83bede94288030e5dcf712ac8e
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-05 02:19:21 -0300

    CalendarUtils: Adicionando métodos convertToCalendar e  isDifferenceGreaterThan180Days
    - convertToCalendar: Converte um tipo Date para um Calendar
    - isDifferenceGreaterThan180Days: Verifica a diferença entre duas datas e retorna true se a diferença for maior/igual a 180 dias em MS

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/CalendarUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/CalendarUtils.java
@@ -2,6 +2,7 @@ package com.tibiadata.tibia_crawler.model.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  *
@@ -20,4 +21,24 @@ public class CalendarUtils {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(calendar.getTime());
     }
+
+    /**
+     * Convert a Date format to Calendar
+     *
+     * @param date Date to be converted to Calendar
+     * @return Calendar
+     */
+    public Calendar convertToCalendar(Date date) {
+        Calendar converted = Calendar.getInstance();
+        converted.setTime(date);
+        return converted;
+    }
+
+    public boolean isDifferenceGreaterThan180Days(Calendar date1, Calendar date2) {
+        long diffInMillis = Math.abs(date1.getTimeInMillis() - date2.getTimeInMillis()); // Calcular a diferença em milissegundos
+        long diffInDays = diffInMillis / (24 * 60 * 60 * 1000); // Converter milissegundos para dias (1 dia = 86400000 milissegundos)
+        return diffInDays >= 180; // Verificar se a diferença é maior ou igual a 180 dias
+    }
+    
+    // TODO método com input string MM/DD/YYYY e retorna um Calendar/Date formatado com essa data
 }
