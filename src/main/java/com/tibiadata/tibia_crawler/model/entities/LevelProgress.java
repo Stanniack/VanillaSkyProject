33168f92852b0fc5366845121488297a76db4c02
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-09 22:27:24 -0300

    LevelProgress: Criando classe para persistir níveis

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/entities/LevelProgress.java
@@ -0,0 +1,72 @@
+package com.tibiadata.tibia_crawler.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Devmachine
+ */
+@Entity
+public class LevelProgress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String level;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id", nullable = false) // FK será associada ao "id" de Personage
+    private Personage personage;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar dayProgress;
+
+    /**
+     * Hibernate needs a default constructor
+     */
+    public LevelProgress() {
+    }
+
+    public LevelProgress(String level, Calendar dayProgress) {
+        this.level = level;
+        this.dayProgress = dayProgress;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Personage getPersonage() {
+        return personage;
+    }
+
+    public void setPersonage(Personage personage) {
+        this.personage = personage;
+    }
+
+    public Calendar getDayProgress() {
+        return dayProgress;
+    }
+
+    public void setDayProgress(Calendar dayProgress) {
+        this.dayProgress = dayProgress;
+    }
+
+}
