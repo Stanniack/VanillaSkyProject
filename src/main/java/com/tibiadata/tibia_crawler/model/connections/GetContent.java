f9852b8a8ae34a258e72444bed59a84f53b75ff4
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-18 13:12:28 -0300

    GetContent: Adicionando método getOnlinePlayers
    - Método getOnlinePlayers retorna uma lista de lista de string contendo o nome, vocação e level do personagem.

--- src/main/java/com/tibiadata/tibia_crawler/model/connections/GetContent.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/connections/GetContent.java
@@ -33,8 +33,28 @@ public class GetContent {
 
         for (Element tr : parserDocument.select(select1)) {
             for (Element td : tr.select(select2)) {
-                allContent.add(td.text()); // Adiciona o texto de cada à lista
+                allContent.add(td.text()); // Adiciona o texto de cada linha à lista
             }
+        }
+        return allContent;
+    }
+
+    public List<List<String>> getOnlinePlayers(String url, String select1, String select2) throws IOException {
+        Document document = Jsoup.connect(url).get(); // Pega o html
+
+        String html = document.toString(); // HTML a ser limpo
+
+        // Fazer parsing do HTML
+        Document parserDocument = Jsoup.parse(html);
+
+        List<List<String>> allContent = new ArrayList<>();
+
+        for (Element tr : parserDocument.select(select1)) {
+            List<String> eachPlayer = new ArrayList<>();
+            for (Element td : tr.select(select2)) {
+                eachPlayer.add(td.text()); // Adiciona a lista contendo a linha a uma segunda linha
+            }
+            allContent.add(eachPlayer);
         }
         return allContent;
     }
