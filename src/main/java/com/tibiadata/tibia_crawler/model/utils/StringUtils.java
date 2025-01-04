e743e1bc46e46687975d6997261ba060a0641cf4
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-04 02:55:59 -0300

    StringUtils: Adicionando método e seu JavaDoc
    - Adicionando método polimórfico splitAndReplace com parâmetro para tamanho do array a ser retornado

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
@@ -14,8 +14,21 @@ public class StringUtils {
      * @param regex the regular expression used to split the string
      * @return an array containing up to two parts of the processed string
      */
-    public static String[] splitAndReplace(String item, String regex) {
+    public String[] splitAndReplace(String item, String regex) {
         return item.split(regex, 2);
+    }
+
+    /**
+     * Splits the input string using the provided regex and returns an array
+     * with a maximum size defined by the given arraySize.
+     *
+     * @param item the string to be split and processed
+     * @param regex the regular expression used to split the string
+     * @param arraySize the maximum number of parts in the resulting array
+     * @return an array containing up to arraySize parts of the processed string
+     */
+    public String[] splitAndReplace(String item, String regex, int arraySize) {
+        return item.split(regex, arraySize);
     }
 
     /**
@@ -23,7 +36,7 @@ public class StringUtils {
      * @param str string to replace first index
      * @return string treated without first index containing space
      */
-    public static String replaceFirstSpace(String str) {
+    public String replaceFirstSpace(String str) {
         return str.replaceFirst("^\\s+", "");
     }
 }
