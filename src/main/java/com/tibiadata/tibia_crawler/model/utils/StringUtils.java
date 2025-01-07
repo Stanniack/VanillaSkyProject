8c2c81d1be49d5ba402b60a4de9b45ba74aa2ec9
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-07 02:48:29 -0300

    StrUtils: Refatorando métodos
    - Desambiguação dos nomes dos métodos

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
@@ -10,24 +10,28 @@ public class StringUtils {
      * Splits the input string using the provided regex and returns an array
      * with at most two parts.
      *
-     * @param item the string to be split and processed
-     * @param regex the regular expression used to split the string
+     * @param item the string to be multSplit and processed
+     * @param regex the regular expression used to multSplit the string
      * @return an array containing up to two parts of the processed string
      */
-    public String[] splitAndReplace(String item, String regex) {
+    public String[] split(String item, String regex) {
         return item.split(regex, 2);
+    }
+    
+    public String[] multSplit(String item, String regex){
+        return item.split(regex);
     }
 
     /**
      * Splits the input string using the provided regex and returns an array
      * with a maximum size defined by the given arraySize.
      *
-     * @param item the string to be split and processed
-     * @param regex the regular expression used to split the string
+     * @param item the string to be multSplit and processed
+     * @param regex the regular expression used to multSplit the string
      * @param arraySize the maximum number of parts in the resulting array
      * @return an array containing up to arraySize parts of the processed string
      */
-    public String[] splitAndReplace(String item, String regex, int arraySize) {
+    public String[] split(String item, String regex, int arraySize) {
         return item.split(regex, arraySize);
     }
 
