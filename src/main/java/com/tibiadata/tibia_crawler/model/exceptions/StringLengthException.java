bc12c0c3d9be72b6440ddee373820a323320deaa
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-09 23:03:56 -0300

    StringLengthException: Exceção para tamanho de string no banco de dados

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/exceptions/StringLengthException.java
@@ -0,0 +1,12 @@
+package com.tibiadata.tibia_crawler.model.exceptions;
+
+/**
+ *
+ * @author Devmachine
+ */
+public class StringLengthException extends Exception {
+
+    public StringLengthException(String message) {
+        super(message);
+    }
+}
