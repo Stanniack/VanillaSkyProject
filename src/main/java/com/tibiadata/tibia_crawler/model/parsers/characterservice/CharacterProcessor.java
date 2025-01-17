635af7b7dddc10a1f9d7433e19d7900861458694
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-17 08:09:46 -0300

    CharacterProcessor: Classe para lidar com ObjectProvider
    - Classe para lidar com ObjectProvider para respeitar o escopo "prototype"

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/parsers/characterservice/CharacterProcessor.java
@@ -0,0 +1,40 @@
+package com.tibiadata.tibia_crawler.model.parsers.characterservice;
+
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Scope;
+
+@Component
+@Scope("prototype")
+public class CharacterProcessor {
+
+    private final ObjectProvider<CharacterService> characterServiceProvider;
+
+    // Construtor para injeção de dependência
+    public CharacterProcessor(ObjectProvider<CharacterService> characterServiceProvider) {
+        this.characterServiceProvider = characterServiceProvider;
+    }
+
+    // Método que recebe uma lista de nomes e faz o processamento
+    public void processNames(List<String> names) throws IOException {
+        for (String name : names) {
+            fetcher(name);
+        }
+    }
+
+    public void processName(String name) throws IOException {
+        fetcher(name);
+    }
+
+    private void fetcher(String name) throws IOException {
+        CharacterService characterService = getCharacterServiceProvider().getObject();
+        characterService.fetchCharacter("https://www.tibia.com/community/?subtopic=characters&name=" + name);
+    }
+
+    private ObjectProvider<CharacterService> getCharacterServiceProvider() {
+        return characterServiceProvider;
+    }
+
+}
