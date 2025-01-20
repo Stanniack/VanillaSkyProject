7b85be09c2ff386f20f21a1da9c0bdb8c7984424
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-20 18:55:48 -0300

    StringUrils: Refatoração nos Javadocs e novo método
    - Refatorando Javadocs para pt-br
    - Método nameFormater que adiciona "+" em nomes compostos

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/StringUtils.java
@@ -1,81 +1,91 @@
-package com.tibiadata.tibia_crawler.model.utils;
 
-/**
- *
- * @author Devmachine
- */
 public class StringUtils {
 
     /**
-     * Splits the input string using the provided regex and returns an array
-     * with at most two parts.
+     * Divide a string de entrada usando a expressão regular fornecida e retorna
+     * um array com no máximo duas partes.
      *
-     * @param item the string to be multSplit and processed
-     * @param regex the regular expression used to multSplit the string
-     * @return an array containing up to two parts of the processed string
+     * @param item a string a ser dividida e processada
+     * @param regex a expressão regular usada para dividir a string
+     * @return um array contendo até duas partes da string processada
      */
     public static String[] split(String item, String regex) {
         return item.split(regex, 2);
     }
 
     /**
-     * Splits the given string by the specified regex pattern.
+     * Divide a string fornecida pela expressão regular especificada.
      *
-     * @param item the string to split
-     * @param regex the regex pattern to use for splitting
-     * @return an array of substrings
+     * @param item a string a ser dividida
+     * @param regex a expressão regular a ser usada para dividir
+     * @return um array de substrings
      */
     public static String[] multSplit(String item, String regex) {
         return item.split(regex);
     }
 
     /**
-     * Splits the input string using the provided regex and returns an array
-     * with a maximum size defined by the given arraySize.
+     * Divide a string de entrada usando a expressão regular fornecida e retorna
+     * um array com um tamanho máximo definido pelo parâmetro arraySize.
      *
-     * @param item the string to be multSplit and processed
-     * @param regex the regular expression used to multSplit the string
-     * @param arraySize the maximum number of parts in the resulting array
-     * @return an array containing up to arraySize parts of the processed string
+     * @param item a string a ser dividida e processada
+     * @param regex a expressão regular usada para dividir a string
+     * @param arraySize o número máximo de partes no array resultante
+     * @return um array contendo até arraySize partes da string processada
      */
     public static String[] split(String item, String regex, int arraySize) {
         return item.split(regex, arraySize);
     }
 
     /**
-     * Removes the first occurrence of leading whitespace from the given string.
+     * Remove a primeira ocorrência de espaço em branco no início da string
+     * fornecida.
      *
-     * @param str the string to process
-     * @return the string without leading whitespace
+     * @param str a string a ser processada
+     * @return a string sem o espaço em branco inicial
      */
     public static String replaceFirstSpace(String str) {
         return str.replaceFirst("^\\s+", "");
     }
 
     /**
-     * Splits the given string by ":" and removes leading whitespace from the
-     * specified index.
+     * Divide a string fornecida por ":" e remove o espaço em branco do início
+     * da substring no índice especificado.
      *
-     * @param item the string to split
-     * @param index the index of the substring to process
-     * @return the processed substring with leading whitespace removed
+     * @param item a string a ser dividida
+     * @param index o índice da substring a ser processada
+     * @return a substring processada, sem o espaço em branco inicial
      */
     public static String splitAndReplace(String item, int index) {
         return replaceFirstSpace(split(item, ":")[index]);
     }
 
     /**
-     * Splits the given string by the specified regex, limits the array size,
-     * and removes leading whitespace from the specified index.
+     * Divide a string fornecida pela expressão regular especificada, limita o
+     * tamanho do array e remove o espaço em branco do início da substring no
+     * índice especificado.
      *
-     * @param item the string to split
-     * @param regex the regex pattern to use for splitting
-     * @param arraySize the maximum size of the resulting array
-     * @param indexItem the index of the substring to process
-     * @return the processed substring with leading whitespace removed
+     * @param item a string a ser dividida
+     * @param regex a expressão regular a ser usada para dividir
+     * @param arraySize o tamanho máximo do array resultante
+     * @param indexItem o índice da substring a ser processada
+     * @return a substring processada, sem o espaço em branco inicial
      */
     public static String splitAndReplace(String item, String regex, int arraySize, int indexItem) {
         return replaceFirstSpace(split(item, regex, arraySize)[indexItem]);
+    }
+
+    /**
+     * Formata um nome, substituindo os espaços por "+".
+     *
+     * @param name O nome a ser formatado.
+     * @return O nome formatado, com espaços substituídos por "+".
+     */
+    public static String nameFormater(String name) {
+        if (name == null || name.isEmpty()) {
+            return name;
+        }
+        return name.replace(" ", "+");
     }
 
 }
