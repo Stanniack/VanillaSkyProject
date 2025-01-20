51e07ce83459ecc9c35e1b988cb61a6415ca44c2
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-20 18:15:48 -0300

    Create BrotliDescompacter.java
    - Classe e método gzipBrDescompacter para descompactar arquivos gzip e br

---
+++ src/main/java/com/tibiadata/tibia_crawler/model/connections/BrotliDescompacter.java
@@ -0,0 +1,49 @@
+package com.tibiadata.tibia_crawler.model.connections;
+
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.zip.GZIPInputStream;
+import org.brotli.dec.BrotliInputStream;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Devmachine
+ */
+@Component
+public class BrotliDescompacter {
+
+    public String gzipBrDescompacter(String url) throws MalformedURLException, IOException {
+        StringBuilder response = null;
+
+        URL link = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) link.openConnection();
+        connection.setRequestProperty("Accept-Encoding", "gzip, br");
+
+        InputStream inputStream = connection.getInputStream();
+        String encoding = connection.getContentEncoding();
+
+        if ("gzip".equalsIgnoreCase(encoding)) {
+            inputStream = new GZIPInputStream(inputStream);
+        } else if ("br".equalsIgnoreCase(encoding)) {
+            inputStream = new BrotliInputStream(inputStream);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line).append("\n");
+            }
+        } finally {
+            connection.disconnect(); // fecha a conexão
+        }
+
+        return response.toString();
+    }
+}
