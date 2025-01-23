c2d340a316988f0b5ff46e9fb6011992b873094d
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-23 20:59:21 -0300

    CalendarUtils: Removendo métodos estáticos não utilizados

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/CalendarUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/CalendarUtils.java
@@ -20,30 +20,7 @@ import java.util.logging.Logger;
  * @author Devmachine
  */
 public class CalendarUtils {
-
-    /**
-     * Retorna a data e hora atual no formato "yyyy-MM-dd HH:mm:ss".
-     *
-     * @return String contendo a data e hora atual formatada.
-     */
-    public static String getDate() {
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return sdf.format(calendar.getTime());
-    }
-
-    /**
-     * Converte um objeto Date para um objeto Calendar.
-     *
-     * @param date Data a ser convertida para Calendar.
-     * @return Calendar correspondente à data fornecida.
-     */
-    public static Calendar convertToCalendar(Date date) {
-        Calendar converted = Calendar.getInstance();
-        converted.setTime(date);
-        return converted;
-    }
-
+    
     /**
      * Compara duas datas para verificar se a diferença entre elas é maior ou
      * igual a 180 dias.
@@ -61,33 +38,11 @@ public class CalendarUtils {
     }
 
     /**
-     * Converte uma string no formato "MM/dd/yyyy" para um objeto Calendar.
-     *
-     * @param date A string representando uma data no formato "MM/dd/yyyy".
-     * @return O objeto Calendar correspondente à data fornecida.
-     */
-    public static Calendar parseDate(String date) {
-        try {
-            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-            sdf.setLenient(false); // Desativa datas improváveis
-            Date parsedDate = sdf.parse(date);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(parsedDate);
-
-            return calendar;
-
-        } catch (ParseException ex) {
-            Logger.getLogger(CalendarUtils.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return Calendar.getInstance();
-    }
-
-    /**
      * Converte uma string representando uma data e hora no formato "MMM d yyyy,
      * HH:mm:ss" para um objeto Calendar, usando o fuso horário "Europe/Paris".
      *
      * @param textDate A string representando a data e hora no formato "MMM d
-     * yyyy, HH:mm:ss".
+     *                 yyyy, HH:mm:ss".
      * @return Um objeto Calendar representando a data e hora convertidas para o
      * fuso horário "Europe/Paris".
      */
@@ -104,25 +59,6 @@ public class CalendarUtils {
         calendar.setTime(date);
 
         return calendar;
-    }
-
-    /**
-     * Compara dois objetos Calendar para verificar se eles representam a mesma
-     * data e hora, incluindo ano, mês, dia, hora, minuto e segundo.
-     *
-     * @param c1 O primeiro objeto Calendar a ser comparado.
-     * @param c2 O segundo objeto Calendar a ser comparado.
-     * @return {@code true} se ambos os objetos Calendar representam a mesma
-     * data e hora, {@code false} caso contrário.
-     */
-    public static boolean isSameDateTime(Calendar c1, Calendar c2) {
-        return c1 != null && c2 != null
-                && c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR)
-                && c1.get(Calendar.MONTH) == c2.get(Calendar.MONTH)
-                && c1.get(Calendar.DAY_OF_MONTH) == c2.get(Calendar.DAY_OF_MONTH)
-                && c1.get(Calendar.HOUR_OF_DAY) == c2.get(Calendar.HOUR_OF_DAY)
-                && c1.get(Calendar.MINUTE) == c2.get(Calendar.MINUTE)
-                && c1.get(Calendar.SECOND) == c2.get(Calendar.SECOND);
     }
 
     /**
