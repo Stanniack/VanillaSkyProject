f4e0b80f82d1634231d1b9c1681301fa3bd838fd
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-26 22:47:23 -0300

    CalendarUtilsTest: Adicionando class
    - Método testGreaterThan180Days que testa greaterThan180Days adicionado.

---
+++ src/test/java/com/tibiadata/tibia_crawler/model/utils/CalendarUtilsTest.java
@@ -0,0 +1,27 @@
+package com.tibiadata.tibia_crawler.model.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CalendarUtilsTest {
+
+    @Test
+    public void testGreaterThan180Days() throws ParseException {
+        String dateStr = "2024-07-30"; // Data no formato yyyy-MM-dd
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date date = sdf.parse(dateStr); // Converte para Date
+        Calendar testCalendar = Calendar.getInstance();
+        testCalendar.setTime(date); // Define a data no Calendar
+
+        assertTrue(CalendarUtils.greaterThan180Days(Calendar.getInstance(), testCalendar), "A diferença entre as data é maior que 180 dias (6 meses)");
+
+    }
+}
