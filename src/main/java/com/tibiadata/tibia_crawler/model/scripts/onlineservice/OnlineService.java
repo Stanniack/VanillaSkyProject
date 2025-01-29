9e769c5727d9b70a7a559305a416313ba9e81e50
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-29 19:54:40 -0300

    OnlineService: Refatorando classe
    - Desacoplando e tornando-a mais legível

--- src/main/java/com/tibiadata/tibia_crawler/model/scripts/onlineservice/OnlineService.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/scripts/onlineservice/OnlineService.java
@@ -2,13 +2,11 @@ package com.tibiadata.tibia_crawler.model.scripts.onlineservice;
 
 import com.tibiadata.tibia_crawler.model.utils.CalendarUtils;
 import com.tibiadata.tibia_crawler.model.utils.StringUtils;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
+import com.tibiadata.tibia_crawler.model.utils.TibiaUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,65 +29,80 @@ public class OnlineService {
     }
 
     //!!! retorna lista contendo total players e off players e só persiste tempo online de total. A persistência dos personagens ocorre em ambas as listas?
-    public Map<String, Map<String, Long>> serversVerifier() {
+    public Map<String, Map<String, Long>> serversVerifier(int minute) {
+        long timer = System.currentTimeMillis();
 
-        while (!CalendarUtils.isCurrentMinute(16)) { // Aguarda até que o minuto atual seja 03
+        while ((System.currentTimeMillis() - timer)/1000 < (minute * 60L)) { // Aguarda o timer chegar no for maior para finalizar
+            onlineTimeManager(TibiaUtils.getExperimentalWorlds());
+        }
 
-            for (String world : Arrays.asList("Zunera")) { // Itera sobre a lista de mundos 
-                long startTime = System.currentTimeMillis();
-                List<List<String>> playersList = onlineWorldPlayersProcessor.processWorld(world); // Obtém a lista de jogadores online
-
-                worldOnlinePlayers.putIfAbsent(world, new HashMap<>());
-                worldTotalPlayers.putIfAbsent(world, new HashMap<>());
-                worldOfflinePlayers.putIfAbsent(world, new HashMap<>());
-
-                for (List<String> players : playersList) { // Itera sobre cada lista de jogadores
-                    String[] infoPlayer = StringUtils.split(String.join(", ", players), "\\d+", 2); // Divide a string do jogador, ignorando level e profissão
-                    String player = infoPlayer[0].trim(); // Trata o nome do jogador (retira espaços)
-
-                    /* Mapa auxiliar para somar tempo online do jogador? Teste de mesa para Samarcarna!!!*/
-                    if (worldTotalPlayers.get(world).get(player) != null) { //Se diferente de nulo ent player já tem entrada
-                        long currentOnlineTime = worldTotalPlayers.get(world).get(player) + (System.currentTimeMillis() - startTime);
-                        worldOnlinePlayers.get(world).put(player, currentOnlineTime);
-
-                    } else {// Senão nova entrada do player
-                        worldOnlinePlayers.get(world).put(player, 0L);
-                    }
-
-                } // fim for mundos
-
-                Iterator<String> iterator = worldTotalPlayers.get(world).keySet().iterator();
-
-                while (iterator.hasNext()) {
-                    String name = iterator.next();
-                    if (!worldOnlinePlayers.get(world).containsKey(name)) { // Se o jogador não estiver mais online
-                        onlineCharacterProcessor.onlineCharacter(name, worldTotalPlayers.get(world).get(name).intValue()); // persistência simultânea
-                        worldOfflinePlayers.get(world).put(name, worldTotalPlayers.get(world).get(name)); // Move o jogador para a lista de offline
-                        iterator.remove();
-                    }
-                }
-
-                worldTotalPlayers.get(world).putAll(worldOnlinePlayers.get(world)); // Atualiza a lista total com os jogadores online do mundo
-                worldOnlinePlayers.get(world).clear(); // Limpa a lista de jogadores online do mundo
-            }
-
-        } // Fim while
-
-        printPlayers();
+        //printPlayers();
 
         return worldTotalPlayers;
     }
 
+    private void onlineTimeManager(List<String> worlds) {
+
+        for (String world : worlds) { // Itera sobre a lista de mundos
+            long startTime = System.currentTimeMillis();
+            List<List<String>> playersList = onlineWorldPlayersProcessor.processWorld(world); // Obtém a lista de jogadores online
+
+            initializeWorldMaps(world);
+
+            for (List<String> players : playersList) { // Itera sobre cada lista de jogadores
+                String[] infoPlayer = StringUtils.split(String.join(", ", players), "\\d+", 2); // Divide a string do jogador, ignorando level e profissão
+                String player = infoPlayer[0].trim(); // Trata o nome do jogador (retira espaços)
+
+                if (worldTotalPlayers.get(world).get(player) != null) { //Se diferente de nulo ent player já tem entrada
+                    long currentOnlineTime = worldTotalPlayers.get(world).get(player) + (System.currentTimeMillis() - startTime);
+                    worldOnlinePlayers.get(world).put(player, currentOnlineTime);
+
+                } else {// Senão nova entrada do player
+                    worldOnlinePlayers.get(world).put(player, 0L);
+                }
+
+            } // fim for mundos
+
+            Iterator<String> iterator = worldTotalPlayers.get(world).keySet().iterator();
+
+            while (iterator.hasNext()) {
+                String name = iterator.next();
+                if (!worldOnlinePlayers.get(world).containsKey(name)) { // Se o jogador não estiver mais online
+                    onlineCharacterProcessor.onlineCharacter(name, worldTotalPlayers.get(world).get(name).intValue()); // !!persistência simultânea
+                    worldOfflinePlayers.get(world).put(name, worldTotalPlayers.get(world).get(name)); // Move o jogador para a lista de offline
+                    iterator.remove();
+                }
+            }
+
+            worldTotalPlayers.get(world).putAll(worldOnlinePlayers.get(world)); // Atualiza a lista total com os jogadores online do mundo
+            worldOnlinePlayers.get(world).clear(); // Limpa a lista de jogadores online do mundo
+        }
+    }
+
+    private void initializeWorldMaps(String world) {
+        worldOnlinePlayers.putIfAbsent(world, new HashMap<>());
+        worldTotalPlayers.putIfAbsent(world, new HashMap<>());
+        worldOfflinePlayers.putIfAbsent(world, new HashMap<>());
+    }
+
     private void printPlayers() {
+
         System.out.println("---------");
+        System.out.println("Online:");
+        System.out.println("---------");
+
         for (var entry : worldTotalPlayers.entrySet()) {
             entry.getValue().entrySet().forEach(System.out::println);
         }
 
         System.out.println("---------");
+        System.out.println("Offline:");
+        System.out.println("---------");
+
         for (var entry : worldOfflinePlayers.entrySet()) {
             entry.getValue().entrySet().forEach(System.out::println);
         }
     }
 
 }
+
