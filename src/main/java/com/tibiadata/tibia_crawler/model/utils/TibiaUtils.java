74332586675e9502cd5d07947599a473f076ada2
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-01-30 01:23:00 -0300

    TibiaUtils: Arrumando categoria de servidores

--- src/main/java/com/tibiadata/tibia_crawler/model/utils/TibiaUtils.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/utils/TibiaUtils.java
@@ -6,13 +6,12 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- *
  * @author Devmachine
  */
 public class TibiaUtils {
 
     @Getter
-    private static final List<String> worlds = Arrays.asList(
+    private static final List<String> allWorlds = Arrays.asList(
             "Aethera", "Ambra", "Antica", "Astera", "Belobra", "Bona", "Bravoria", "Calmera",
             "Cantabra", "Celebra", "Celesta", "Collabra", "Descubra", "Dia", "Divina", "Epoca",
             "Esmera", "Etebra", "Ferobra", "Fibera", "Firmera", "Flamera", "Gentebra", "Gladera",
@@ -30,50 +29,31 @@ public class TibiaUtils {
             "Zuna", "Zunera");
 
     @Getter
-    private static final List<String> pvpServers = Arrays.asList(
-            "Aethera", "Antica", "Cantabra", "Ferobra", "Honbra", "Lobera", "Havera", "Ombra", "Ourobra",
-            "Peloria", "Quebra", "Quelibra", "Rasteibra", "Solidera", "Tornabra", "Secura", "Serdebra", "Wintera"
+    private static final List<String> openPvpWorlds = Arrays.asList(
+            "Aethera", "Antica", "Cantabra", "Ferobra", "Havera", "Honbra", "Inabra", "Jadebra", "Jaguna",
+            "Lobera", "Ombra", "Ourobra", "Peloria", "Premia", "Quelibra", "Quidera", "Quintera", "Rasteibra",
+            "Runera", "Serdebra", "Solidera", "Talera", "Thyria", "Tornabra", "Unebra", "Vandera", "Victoris",
+            "Vunira", "Wintera", "Xyla", "Yara"
     );
 
     @Getter
-    private static final List<String> optPvpServers = Arrays.asList(
-            "Astera", "Belobra", "Bona", "Bravoria", "Calmera", "Celebra", "Celesta", "Collabra", "Descubra",
-            "Esmera", "Etebra", "Fibera", "Firmera", "Gentebra", "Gladera", "Harmonia", "Issobra", "Jacabra",
-            "Jadebra", "Kalibra", "Karmeya", "Luminera", "Menera", "Nefera", "Nevia", "Monza", "Neiva", "Obscubra",
-            "Oceanis", "Ombra", "Pacera", "Refugia", "Secura", "Ustebra", "Venebra", "Yovera", "Yubra", "Zephyra"
+    private static final List<String> optionalPvpWorlds = Arrays.asList(
+            "Astera", "Belobra", "Bona", "Bravoria", "Calmera", "Celebra", "Celesta", "Collabra",
+            "Descubra", "Dia", "Esmera", "Etebra", "Gentebra", "Gladera", "Harmonia", "Issobra",
+            "Kalibra", "Karmeya", "Luminera", "Menera", "Monza", "Nefera", "Nevia", "Oceanis", "Pacera",
+            "Refugia", "Secura", "Stralis", "Ulera", "Ustebra", "Venebra", "Vitera", "Wadira", "Yonabra",
+            "Yovera", "Yubra", "Zephyra"
     );
 
     @Getter
     private static final List<String> retroOpenPvpServers = Arrays.asList(
-            "Ambra", "Divina", "Epoca", "Fibera", "Flamera", "Gladibra", "Lutabra", "Malivora", "Lutabra",
-            "Quebra", "Temera"
+            "Ambra", "Divina", "Epoca", "Fibera", "Firmera", "Flamera", "Gladibra", "Lutabra",
+            "Malivora", "Quebra", "Temera"
     );
 
     @Getter
     private static final List<String> retroHardcorePvpservers = Arrays.asList(
-            "Gravitera", "Jacabra", "Obscubra", "Retalia", "Zunera"
-    );
-
-    @Getter
-    private static final List<String> southAmericaServers = Arrays.asList(
-            "Ambra", "Belobra", "Cantabra", "Celebra", "Collabra", "Descubra", "Ferobra", "Gentebra", "Havera",
-            "Honbra", "Inabra", "Issobra", "Jacabra", "Jadebra", "Kalibra", "Lutabra", "Malivora", "Menera",
-            "Monza", "Neiva", "Ombra", "Pacera", "Quebra", "Quelibra", "Rasteibra", "Serdebra", "Tornabra", "Unebra",
-            "Ustebra", "Venebra", "Yara", "Yonabra", "Yubra"
-    );
-
-    @Getter
-    private static final List<String> northAmericaServers = Arrays.asList(
-            "Aethera", "Astera", "Calmera", "Celebra", "Fibera", "Firmera", "Gladera", "Gravitera", "Havera",
-            "Lobera", "Menera", "Nefera", "Quidera", "Quintera", "Runera", "Solidera", "Talera", "Temera", "Ulera",
-            "Vandera", "Vitera", "Wintera"
-    );
-
-    @Getter
-    private static final List<String> europeServers = Arrays.asList(
-            "Antica", "Bona", "Bravoria", "Celesta", "Dia", "Divina", "Epoca", "Harmonia", "Jaguna", "Karmeya",
-            "Luminera", "Monza", "Neiva", "Nevia", "Peloria", "Premia", "Thyria", "Vunira", "Wadira", "Xyla", "Yara",
-            "Zephyra", "Zuna"
+            "Gravitera", "Jacabra", "Obscubra", "Retalia", "Wildera"
     );
 
 }
