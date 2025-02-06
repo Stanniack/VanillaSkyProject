77ccbc1e8914823a9fb43d116baf3024e1cc7963
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2025-02-06 17:17:38 -0300

    OnlineCharactersPersistence: Adicionando debug

--- src/main/java/com/tibiadata/tibia_crawler/model/scripts/onlineservice/OnlineCharactersPersistence.java
+++ src/main/java/com/tibiadata/tibia_crawler/model/scripts/onlineservice/OnlineCharactersPersistence.java
@@ -1,5 +1,8 @@
 package com.tibiadata.tibia_crawler.model.scripts.onlineservice;
 
+import com.tibiadata.tibia_crawler.model.scripts.serversservice.ServersVerifierFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +10,8 @@ import java.util.Map;
 
 @Component
 public class OnlineCharactersPersistence {
+    private static final Logger logger = LoggerFactory.getLogger(OnlineCharactersPersistence.class);
+    //
     private final OnlineCharacterProcessor onlineCharacterProcessor;
 
     @Autowired
@@ -15,7 +20,9 @@ public class OnlineCharactersPersistence {
     }
 
     public void serverSaveCharactersPersistence(Map<String, Map<String, Long>> worldTotalPlayers) {
+
         for (var worldEntry : worldTotalPlayers.entrySet()) {// mundos
+            logger.debug("SALVANDO PERSONAGENS NO SERVER SAVE DO SERVIDOR: {}", worldEntry.getKey());
             for (var playerEntry : worldEntry.getValue().entrySet()) {//personagens
                 onlineCharacterProcessor.onlineCharacter(playerEntry.getKey(), playerEntry.getValue().intValue());
             }
